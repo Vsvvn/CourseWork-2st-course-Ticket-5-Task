6e110d1cec091aee705b33f59dd97b315aee5392
Vsvvn <Vladsun@yandex.ru>
2023-01-12 20:10:05 +0300

    Задача решена.

---
+++ src/Number.java
@@ -0,0 +1,20 @@
+
+
+public enum Number {
+
+    ONE(1),
+    TWO(2),
+    THREE(3);
+
+    private final int number;
+
+    Number(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
+
+
